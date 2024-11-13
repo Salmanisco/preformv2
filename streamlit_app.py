368855d1851f21b4cac0397d2810482e5b235911
Salmanisco <salmanisco@gmail.com>
2024-11-13 17:11:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,133 @@
 import streamlit as st
+import tensorflow as tf
+import pathlib
+import os
+from tensorflow import keras
+import numpy as np
+import pickle
+import random
+import time
+from PIL import Image
+import gdown
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title="Preform Classifier",
+    page_icon="Logo/machine-learning.png",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'About': "This app is a demonstration of the preform classification model trained using a Convolutional Neural Network"
+    }
 )
+
+st.logo(image='Logo/machine-learning.png', 
+        icon_image='Logo/machine-learning.png')
+
+
+def pad_image_to_size(image_id, target_size=(512, 512)):
+    img = tf.keras.preprocessing.image.load_img(image_id)
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    height, width, channels = img_array.shape  # Get channels
+
+    # Calculate padding
+    pad_height = max(0, target_size[1] - height)
+    pad_width = max(0, target_size[0] - width)
+
+    # Pad the image
+    padded_img = tf.image.pad_to_bounding_box(
+        img_array,
+        pad_height // 2,  # Top padding
+        pad_width // 2,   # Left padding
+        target_size[1],   # Target height
+        target_size[0]    # Target width
+    )
+
+    # Normalize the image
+    normalized_img = padded_img / 255.0
+
+    return tf.keras.preprocessing.image.array_to_img(normalized_img)
+
+def predict_class(image):
+    array = tf.keras.preprocessing.image.img_to_array(image)
+    array = array / 255.0  # Normalize pixel values
+    image = np.expand_dims(array, axis=0)
+    return label_mapping[np.argmax(model.predict(image))]
+
+def random_image(directory = 'test_img'):
+    image_files = [f for f in os.listdir(directory) if f.endswith(('.jpg', '.jpeg', '.png', '.gif'))]
+    random_image = random.choice(image_files)
+    return os.path.join(directory, random_image)
+
+
+def get_class(image):
+    return image.split('-')[0].split("/")[-1]
+
+@st.cache_resource
+def load_model(model_path = 'Model/my_model.keras'):
+    return tf.keras.models.load_model(model_path)
+
+model = load_model()
+
+label_mapping = {
+    0: 'Blackspot',
+    1: 'Degrade',
+    2: 'Good',
+    3: 'Haziness',
+    4: 'Watermark'
+}
+
+
+#Get random image
+if 'random_image' not in st.session_state:
+    st.session_state['random_image'] = random_image()
+
+#procsess image for prediction using model
+processed_img = pad_image_to_size(st.session_state.random_image)
+
+
+
+st.title("Preform Classifier :camera:")
+ 
+left, right = st.columns(2)
+
+
+#Display random image with caption
+with st.container():
+    with left:
+        shuffle = st.button("New image", icon = "🔀")
+        if shuffle:
+            st.session_state['random_image'] = random_image()
+        st.image(st.session_state.random_image, caption=get_class(st.session_state.random_image))
+
+
+    with right:
+        predict = st.button("Predict", icon="🤖")
+        st.header("Prediction:")
+        if predict:
+            with st.spinner("Predicting..."):
+                time.sleep(1)
+            st.header(f"_**{predict_class(processed_img)}**_")
+        
+
+
+st.divider()
+st.subheader("Try it yourself!")
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+
+if picture:
+    # To read image file buffer as a PIL Image:
+    img = Image.open(picture)
+
+    # To convert PIL Image to numpy array:
+    user_image = np.array(img)
+
+    #predict
+    user_predict = st.button("Predict", icon = '🤖', key="user_predict")
+
+    if user_predict:
+        
+        with st.spinner("Predicting..."):
+            time.sleep(1)
+
+        st.header(f"_**{predict_class(user_image)}**_")
